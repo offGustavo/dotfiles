8145c7b32336a4237b8852a5cc067cd71d28212c
Gustavo <geleize@gmail.com>
2025-03-04 13:54:58 -0300

    Add: Rofi combi mode script and add more options in monitor script

--- i3wm/.config/i3/scripts/monitors.sh
+++ i3wm/.config/i3/scripts/monitors.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
-chosen=$(echo -e "Ativar Externo Esquerda\nAtivar Externo Acima\nDesativar Externo\nAtivar Interno\nDesativar Interno\nEspelhar" | rofi -theme "~/.config/i3/rofi/config.rasi" -dmenu -p "Escolha uma opção:")
+chosen=$(echo -e "Ativar Externo Esquerda\nAtivar Externo Acima\nDesativar Externo\nAtivar Interno\nDesativar Interno\nEspelhar\nApenas Externo\nApenas Interno" | rofi -theme "~/.config/i3/rofi/config.rasi" -dmenu -p "Escolha uma opção:")
 
 case "$chosen" in
-"Ativar Externo Esquerda")
-  xrandr --output HDMI-1-0 --mode 1920x1080 --preferred --left-of eDP-1
-  ;;
-"Ativar Externo Acima")
-  xrandr --output HDMI-1-0 --mode 1920x1080 --preferred --above eDP-1
-  ;;
-"Desativar Externo")
-  xrandr --output HDMI-1-0 --off
-  ;;
-"Desativar Interno")
-  xrandr --output eDP-1 --off
-  ;;
-"Ativar Interno")
-  xrandr --output eDP-1 --mode 1920x1080 --right-of HDMI-1-0
-  ;;
-"Espelhar")
-  xrandr --output HDMI-1-0 --same-as eDP-1
-  ;;
-*)
-  echo "Opção inválida"
-  ;;
+    "Ativar Externo Esquerda")
+        xrandr --output HDMI-1-0 --mode 1920x1080 --preferred --left-of eDP-1
+        ;;
+    "Ativar Externo Acima")
+        xrandr --output HDMI-1-0 --mode 1920x1080 --preferred --above eDP-1
+        ;;
+    "Desativar Externo")
+        xrandr --output HDMI-1-0 --off
+        ;;
+    "Desativar Interno")
+        xrandr --output eDP-1 --off
+        ;;
+    "Ativar Interno")
+        xrandr --output eDP-1 --mode 1920x1080 --right-of HDMI-1-0
+        ;;
+    "Espelhar")
+        xrandr --output HDMI-1-0 --same-as eDP-1
+        ;;
+    "Apenas Externo")
+        xrandr --output eDP-1 --off --output HDMI-1-0 --mode 1920x1080
+        ;;
+    "Apenas Interno")
+        xrandr --output HDMI-1-0 --off --output eDP-1 --mode 1920x1080
+        ;;
+    *)
+        echo "Opção inválida"
+        ;;
 esac
-sac
