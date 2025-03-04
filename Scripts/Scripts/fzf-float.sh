aa7230085131593728caf53ee322515497086302
Gustavo <geleize@gmail.com>
2025-03-04 14:15:32 -0300

    RM: Remove Fzf-float.sh

--- Scripts/Scripts/fzf-float.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# alacritty --class 'alacritty-float,alacritty-float' -e ~/Projetos/scripts/monitors.sh
-# alacritty --class 'alacritty-float,alacritty-float' -e ~/Projetos/scripts/sound.sh
-# alacritty --class 'alacritty-float,alacritty-float' -e ~/Projetos/scripts/bluetooth.sh
-
-# gnome-terminal -- $(ls /home/gustavo/Scripts/ | fzf)
