e43fd4b0333288894c8dcf90e612025c8e9567e2
Luiz Gustavo <luizgmdcm@gmail.com>
2025-03-22 20:22:07 -0300

    Add: Add a second panel in dashboard

--- neovim/.config/nvim/lua/plugins/dashboard.lua
+++ neovim/.config/nvim/lua/plugins/dashboard.lua
@@ -1,4 +1,3 @@
--- return {}
 return {
   "snacks.nvim",
   opts = {
@@ -30,6 +29,53 @@ return {
           { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
           { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+      sections = {
+        { section = "header" },
+        {
+          pane = 2,
+          section = "startup",
+          padding = { 1, 0 },
+        },
+        {
+          pane = 2,
+          section = "terminal",
+          -- cmd = "pokeget --hide-name bulbasaur",
+          cmd = "colorscript -e square",
+          -- cmd = "colorscript -e pacman",
+          -- cmd = "chafa ~/Pictures/Wallpapers/my/gengara.jpg --format symbols --symbols vhalf --size 60x11 --stretch",
+          -- cmd = "fzf",
+          height = 7,
+          padding = { 2, 0 },
+          -- indent = 10,
+        },
+        -- {
+        --   pane = 2,
+        --   icon = " ",
+        --   desc = "Browse Repo",
+        --   padding = 1,
+        --   key = "b",
+        --   action = function()
+        --     Snacks.gitbrowse()
+        --   end,
+        -- },
+        { section = "keys", gap = 1, padding = 1 },
+        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        {
+          pane = 2,
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = "git status --short --branch --renames",
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
         },
       },
     },
