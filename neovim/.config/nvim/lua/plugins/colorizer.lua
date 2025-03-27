a535b31a5b06ac19af934cc110a4f1d0040d828a
Luiz Gustavo <luizgmdcm@gmail.com>
2025-03-27 18:06:50 -0300

    CH: Change from nvim-colorizer to nvim-highlight-colors

--- neovim/.config/nvim/lua/plugins/colorizer.lua
+++ neovim/.config/nvim/lua/plugins/colorizer.lua
@@ -1,41 +1,81 @@
 return {
-  "norcalli/nvim-colorizer.lua",
+  "brenoprata10/nvim-highlight-colors",
   config = function()
-    require("colorizer").setup({
-      DEFAULT_OPTIONS = {
-        RGB = true, -- #RGB hex codes
-        RRGGBB = true, -- #RRGGBB hex codes
-        names = false, -- "Name" codes like Blue
-        RRGGBBAA = true, -- #RRGGBBAA hex codes
-        rgb_fn = true, -- CSS rgb() and rgba() functions
-        hsl_fn = true, -- CSS hsl() and hsla() functions
-        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-        -- Available modes: foreground, background
-        mode = "background", -- Set the display mode.
-      },
+    require("nvim-highlight-colors").setup({
+      ---Render style
+      ---@usage 'background'|'foreground'|'virtual'
+      render = "background",
+
+      ---Set virtual symbol (requires render to be set to 'virtual')
+      virtual_symbol = "■",
+
+      ---Set virtual symbol suffix (defaults to '')
+      virtual_symbol_prefix = "",
+
+      ---Set virtual symbol suffix (defaults to ' ')
+      virtual_symbol_suffix = " ",
+
+      ---Set virtual symbol position()
+      ---@usage 'inline'|'eol'|'eow'
+      ---inline mimics VS Code style
+      ---eol stands for `end of column` - Recommended to set `virtual_symbol_suffix = ''` when used.
+      ---eow stands for `end of word` - Recommended to set `virtual_symbol_prefix = ' ' and virtual_symbol_suffix = ''` when used.
+      virtual_symbol_position = "inline",
+
+      ---Highlight hex colors, e.g. '#FFFFFF'
+      enable_hex = true,
+
+      ---Highlight short hex colors e.g. '#fff'
+      enable_short_hex = true,
+
+      ---Highlight rgb colors, e.g. 'rgb(0 0 0)'
+      enable_rgb = true,
+
+      ---Highlight hsl colors, e.g. 'hsl(150deg 30% 40%)'
+      enable_hsl = true,
+
+      ---Highlight ansi colors, e.g '\033[0;34m'
+      enable_ansi = true,
+
+      -- Highlight hsl colors without function, e.g. '--foreground: 0 69% 69%;'
+      enable_hsl_without_function = true,
+
+      ---Highlight CSS variables, e.g. 'var(--testing-color)'
+      enable_var_usage = true,
+
+      ---Highlight named colors, e.g. 'green'
+      enable_named_colors = true,
+
+      ---Highlight tailwind colors, e.g. 'bg-blue-500'
+      enable_tailwind = false,
+
+      ---Set custom colors
+      ---Label must be properly escaped with '%' to adhere to `string.gmatch`
+      --- :help string.gmatch
+      -- custom_colors = {
+      --   { label = "%-%-theme%-primary%-color", color = "#0f1219" },
+      --   { label = "%-%-theme%-secondary%-color", color = "#5a5d64" },
+      -- },
+
+      -- Exclude filetypes or buftypes from highlighting e.g. 'exclude_buftypes = {'text'}'
+      exclude_filetypes = {},
+      exclude_buftypes = {},
+      -- Exclude buffer from highlighting e.g. 'exclude_buffer = function(bufnr) return vim.fn.getfsize(vim.api.nvim_buf_get_name(bufnr)) > 1000000 end'
+      exclude_buffer = function(bufnr) end,
     })
 
-    -- Colorizer Config
-    vim.keymap.set(
-      "n",
-      "<leader>oca",
-      "<Cmd>ColorizerAttachToBuffer<Cr>",
-      { silent = true, desc = "Colorizer Attach To Buffer" }
-    )
-    vim.keymap.set(
-      "n",
-      "<leader>ocr",
-      "<Cmd>ColorizerReloadAllBuffers<Cr>",
-      { silent = true, desc = "Colorizer Reload All Buffers" }
-    )
-    vim.keymap.set(
-      "n",
-      "<leader>ocd",
-      "<Cmd>ColorizerDetachFromBuffer<Cr>",
-      { silent = true, desc = "Colorizer Detach To Buffer" }
-    )
-
-    vim.keymap.set("n", "<leader>oct", "<Cmd>ColorizerToggle<Cr>", { silent = true, desc = "Colorizer Toggle" })
+    vim.keymap.set("n", "<leader>oca", function()
+      require("nvim-highlight-colors").turnOn()
+    end, { silent = true, desc = "Colorizer Attach To Buffer" })
+    vim.keymap.set("n", "<leader>oct", function()
+      require("nvim-highlight-colors").toggle()
+    end, { silent = true, desc = "Colorizer Toggle" })
+    vim.keymap.set("n", "<leader>ocr", function()
+      require("nvim-highlight-colors").turnOff()
+      require("nvim-highlight-colors").turnOn()
+    end, { silent = true, desc = "Colorizer Reload All Buffers" })
+    vim.keymap.set("n", "<leader>ocd", function()
+      require("nvim-highlight-colors").turnOff()
+    end, { silent = true, desc = "Colorizer Detach To Buffer" })
   end,
 }
