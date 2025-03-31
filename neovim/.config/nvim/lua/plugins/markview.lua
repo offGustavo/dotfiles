f7daf1162d53854c58abdd6cdf644f84e7b49a6f
Luiz Gustavo <luizgmdcm@gmail.com>
2025-03-31 01:26:36 -0300

    CL: Clean config

--- neovim/.config/nvim/lua/plugins/markview.lua
+++ neovim/.config/nvim/lua/plugins/markview.lua
@@ -1,30 +1,59 @@
--- return {}
-
 return {
   "OXY2DEV/markview.nvim",
   lazy = false,
-  config = function()
-    require("markview").setup({
-      preview = {
-        modes = { "n", "no", "c" },
-        hybrid_modes = { "n", "v" },
-        linewise_hybrid_mode = true,
-      },
-    })
+  opts = {
+    preview = {
+      modes = { "n", "no", "c" },
+      hybrid_modes = { "n" },
+      linewise_hybrid_mode = true,
+    },
+    -- list_items
+    list_items = {
+      enable = true,
+      wrap = false,
 
-    -- -- Markview Keymaps
-    vim.keymap.set({ "n", "v" }, "<leader>omm", "<cmd>Markview Toggle<CR>", { silent = true, desc = "Toggle Markview" })
-    vim.keymap.set(
-      { "n", "v" },
-      "<leader>omh",
-      "<cmd>Markview hybridToggle<CR>",
-      { silent = true, desc = "Toggle Hybrid Mode" }
-    )
-    vim.keymap.set(
-      { "n", "v" },
-      "<leader>omp",
-      "<cmd>Markview splitToggle<CR>",
-      { silent = true, desc = "Toggle Split View" }
-    )
-  end,
+      indent_size = 2,
+      shift_width = 4,
+
+      marker_minus = {
+        add_padding = true,
+        conceal_on_checkboxes = true,
+
+        text = "",
+        hl = "MarkviewListItemMinus",
+      },
+
+      marker_plus = {
+        add_padding = true,
+        conceal_on_checkboxes = true,
+
+        text = "",
+        hl = "MarkviewListItemPlus",
+      },
+
+      marker_star = {
+        add_padding = true,
+        conceal_on_checkboxes = true,
+
+        text = "",
+        hl = "MarkviewListItemStar",
+      },
+
+      marker_dot = {
+        add_padding = true,
+        conceal_on_checkboxes = true,
+      },
+
+      marker_parenthesis = {
+        add_padding = true,
+        conceal_on_checkboxes = true,
+      },
+    },
+  },
+
+  keys = {
+    { "<leader>omm", "<Cmd>Markview Toggle<Cr>", { desc = "Toggle Markview" } },
+    { "<leader>omh", "<Cmd>Markview hybridToggle<Cr>", { desc = "Toggle Hybrid Mode" } },
+    { "<leader>omp", "<Cmd>Markview splitToggle<Cr>", { desc = "Toggle Split View" } },
+  },
 }
