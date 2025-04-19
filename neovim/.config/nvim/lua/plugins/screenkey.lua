9dcf7d077c4771983a46d48b857eac441dab1ad1
Luiz Gustavo <luizgmdcm@gmail.com>
2025-04-19 00:36:12 -0300

    ADD: Add Screenkey plugin

---
+++ neovim/.config/nvim/lua/plugins/screenkey.lua
@@ -0,0 +1,8 @@
+return {
+  "NStefan002/screenkey.nvim",
+  lazy = false,
+  version = "*", -- or branch = "main", to use the latest commit
+  keys = {
+    { "<leader>ok", "<Cmd>Screenkey<Cr>", { desc = "Screenkey Toggle" } },
+  },
+}
