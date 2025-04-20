9c07cd1ca18c03afd794b6c1207bc9ec22e4676f
Luiz Gustavo <luizgmdcm@gmail.com>
2025-04-20 20:24:54 -0300

    CH: Remove some plugins

--- neovim/.config/lvim/init.lua
+++ neovim/.config/lvim/init.lua
@@ -1979,125 +1979,125 @@ require('lazy').setup(
       version = false,
     },
 
-    -- Colorizer
-    {
-      'brenoprata10/nvim-highlight-colors',
-      config = function()
-        require('nvim-highlight-colors').setup {
-          ---Render style
-          ---@usage 'background'|'foreground'|'virtual'
-          render = 'background',
-          ---Set virtual symbol (requires render to be set to 'virtual')
-          virtual_symbol = '■',
-          ---Set virtual symbol suffix (defaults to '')
-          virtual_symbol_prefix = '',
-          ---Set virtual symbol suffix (defaults to ' ')
-          virtual_symbol_suffix = ' ',
-          ---Set virtual symbol position()
-          ---@usage 'inline'|'eol'|'eow'
-          ---inline mimics VS Code style
-          ---eol stands for `end of column` - Recommended to set `virtual_symbol_suffix = ''` when used.
-          ---eow stands for `end of word` - Recommended to set `virtual_symbol_prefix = ' ' and virtual_symbol_suffix = ''` when used.
-          virtual_symbol_position = 'inline',
-          ---Highlight hex colors, e.g. '#FFFFFF'
-          enable_hex = true,
-          ---Highlight short hex colors e.g. '#fff'
-          enable_short_hex = true,
-          ---Highlight rgb colors, e.g. 'rgb(0 0 0)'
-          enable_rgb = true,
-          ---Highlight hsl colors, e.g. 'hsl(150deg 30% 40%)'
-          enable_hsl = true,
-          ---Highlight ansi colors, e.g '\033[0;34m'
-          enable_ansi = true,
-          -- Highlight hsl colors without function, e.g. '--foreground: 0 69% 69%;'
-          enable_hsl_without_function = true,
-          ---Highlight CSS variables, e.g. 'var(--testing-color)'
-          enable_var_usage = true,
-          ---Highlight named colors, e.g. 'green'
-          enable_named_colors = true,
-          ---Highlight tailwind colors, e.g. 'bg-blue-500'
-          enable_tailwind = false,
-          ---Set custom colors
-          ---Label must be properly escaped with '%' to adhere to `string.gmatch`
-          --- :help string.gmatch
-          -- custom_colors = {
-          --   { label = "%-%-theme%-primary%-color", color = "#0f1219" },
-          --   { label = "%-%-theme%-secondary%-color", color = "#5a5d64" },
-          -- },
-          -- Exclude filetypes or buftypes from highlighting e.g. 'exclude_buftypes = {'text'}'
-          exclude_filetypes = {},
-          exclude_buftypes = {},
-          -- Exclude buffer from highlighting e.g. 'exclude_buffer = function(bufnr) return vim.fn.getfsize(vim.api.nvim_buf_get_name(bufnr)) > 1000000 end'
-          exclude_buffer = function(bufnr) end,
-        }
-        vim.keymap.set('n', '<leader>oca', function()
-          require('nvim-highlight-colors').turnOn()
-        end, { silent = true, desc = 'Colorizer Attach To Buffer' })
-        vim.keymap.set('n', '<leader>oct', function()
-          require('nvim-highlight-colors').toggle()
-        end, { silent = true, desc = 'Colorizer Toggle' })
-        vim.keymap.set('n', '<leader>ocr', function()
-          require('nvim-highlight-colors').turnOff()
-          require('nvim-highlight-colors').turnOn()
-        end, { silent = true, desc = 'Colorizer Reload All Buffers' })
-        vim.keymap.set('n', '<leader>ocd', function()
-          require('nvim-highlight-colors').turnOff()
-        end, { silent = true, desc = 'Colorizer Detach To Buffer' })
-      end,
-    },
+    -- -- Colorizer
+    -- {
+    --   'brenoprata10/nvim-highlight-colors',
+    --   config = function()
+    --     require('nvim-highlight-colors').setup {
+    --       ---Render style
+    --       ---@usage 'background'|'foreground'|'virtual'
+    --       render = 'background',
+    --       ---Set virtual symbol (requires render to be set to 'virtual')
+    --       virtual_symbol = '■',
+    --       ---Set virtual symbol suffix (defaults to '')
+    --       virtual_symbol_prefix = '',
+    --       ---Set virtual symbol suffix (defaults to ' ')
+    --       virtual_symbol_suffix = ' ',
+    --       ---Set virtual symbol position()
+    --       ---@usage 'inline'|'eol'|'eow'
+    --       ---inline mimics VS Code style
+    --       ---eol stands for `end of column` - Recommended to set `virtual_symbol_suffix = ''` when used.
+    --       ---eow stands for `end of word` - Recommended to set `virtual_symbol_prefix = ' ' and virtual_symbol_suffix = ''` when used.
+    --       virtual_symbol_position = 'inline',
+    --       ---Highlight hex colors, e.g. '#FFFFFF'
+    --       enable_hex = true,
+    --       ---Highlight short hex colors e.g. '#fff'
+    --       enable_short_hex = true,
+    --       ---Highlight rgb colors, e.g. 'rgb(0 0 0)'
+    --       enable_rgb = true,
+    --       ---Highlight hsl colors, e.g. 'hsl(150deg 30% 40%)'
+    --       enable_hsl = true,
+    --       ---Highlight ansi colors, e.g '\033[0;34m'
+    --       enable_ansi = true,
+    --       -- Highlight hsl colors without function, e.g. '--foreground: 0 69% 69%;'
+    --       enable_hsl_without_function = true,
+    --       ---Highlight CSS variables, e.g. 'var(--testing-color)'
+    --       enable_var_usage = true,
+    --       ---Highlight named colors, e.g. 'green'
+    --       enable_named_colors = true,
+    --       ---Highlight tailwind colors, e.g. 'bg-blue-500'
+    --       enable_tailwind = false,
+    --       ---Set custom colors
+    --       ---Label must be properly escaped with '%' to adhere to `string.gmatch`
+    --       --- :help string.gmatch
+    --       -- custom_colors = {
+    --       --   { label = "%-%-theme%-primary%-color", color = "#0f1219" },
+    --       --   { label = "%-%-theme%-secondary%-color", color = "#5a5d64" },
+    --       -- },
+    --       -- Exclude filetypes or buftypes from highlighting e.g. 'exclude_buftypes = {'text'}'
+    --       exclude_filetypes = {},
+    --       exclude_buftypes = {},
+    --       -- Exclude buffer from highlighting e.g. 'exclude_buffer = function(bufnr) return vim.fn.getfsize(vim.api.nvim_buf_get_name(bufnr)) > 1000000 end'
+    --       exclude_buffer = function(bufnr) end,
+    --     }
+    --     vim.keymap.set('n', '<leader>oca', function()
+    --       require('nvim-highlight-colors').turnOn()
+    --     end, { silent = true, desc = 'Colorizer Attach To Buffer' })
+    --     vim.keymap.set('n', '<leader>oct', function()
+    --       require('nvim-highlight-colors').toggle()
+    --     end, { silent = true, desc = 'Colorizer Toggle' })
+    --     vim.keymap.set('n', '<leader>ocr', function()
+    --       require('nvim-highlight-colors').turnOff()
+    --       require('nvim-highlight-colors').turnOn()
+    --     end, { silent = true, desc = 'Colorizer Reload All Buffers' })
+    --     vim.keymap.set('n', '<leader>ocd', function()
+    --       require('nvim-highlight-colors').turnOff()
+    --     end, { silent = true, desc = 'Colorizer Detach To Buffer' })
+    --   end,
+    -- },
 
-    -- Markview
-    {
-      'OXY2DEV/markview.nvim',
-      lazy = false,
-      opts = {
-        preview = {
-          modes = { 'n', 'no', 'c' },
-          hybrid_modes = { 'n' },
-          linewise_hybrid_mode = true,
-        },
-        -- list_items
-        markdown = {
-          list_items = {
-            enable = true,
-            wrap = false,
-            indent_size = 2,
-            shift_width = 4,
-            marker_minus = {
-              add_padding = true,
-              conceal_on_checkboxes = true,
-              text = '',
-              hl = 'MarkviewListItemMinus',
-            },
-            marker_plus = {
-              add_padding = true,
-              conceal_on_checkboxes = true,
-              text = '',
-              hl = 'MarkviewListItemPlus',
-            },
-            marker_star = {
-              add_padding = true,
-              conceal_on_checkboxes = true,
-              text = '',
-              hl = 'MarkviewListItemStar',
-            },
-            marker_dot = {
-              add_padding = true,
-              conceal_on_checkboxes = true,
-            },
-            marker_parenthesis = {
-              add_padding = true,
-              conceal_on_checkboxes = true,
-            },
-          },
-        },
-      },
-      keys = {
-        { '<leader>omm', '<Cmd>Markview Toggle<Cr>', { desc = 'Toggle Markview' } },
-        { '<leader>omh', '<Cmd>Markview hybridToggle<Cr>', { desc = 'Toggle Hybrid Mode' } },
-        { '<leader>omp', '<Cmd>Markview splitToggle<Cr>', { desc = 'Toggle Split View' } },
-      },
-    },
+    -- -- Markview
+    -- {
+    --   'OXY2DEV/markview.nvim',
+    --   lazy = false,
+    --   opts = {
+    --     preview = {
+    --       modes = { 'n', 'no', 'c' },
+    --       hybrid_modes = { 'n' },
+    --       linewise_hybrid_mode = true,
+    --     },
+    --     -- list_items
+    --     markdown = {
+    --       list_items = {
+    --         enable = true,
+    --         wrap = false,
+    --         indent_size = 2,
+    --         shift_width = 4,
+    --         marker_minus = {
+    --           add_padding = true,
+    --           conceal_on_checkboxes = true,
+    --           text = '',
+    --           hl = 'MarkviewListItemMinus',
+    --         },
+    --         marker_plus = {
+    --           add_padding = true,
+    --           conceal_on_checkboxes = true,
+    --           text = '',
+    --           hl = 'MarkviewListItemPlus',
+    --         },
+    --         marker_star = {
+    --           add_padding = true,
+    --           conceal_on_checkboxes = true,
+    --           text = '',
+    --           hl = 'MarkviewListItemStar',
+    --         },
+    --         marker_dot = {
+    --           add_padding = true,
+    --           conceal_on_checkboxes = true,
+    --         },
+    --         marker_parenthesis = {
+    --           add_padding = true,
+    --           conceal_on_checkboxes = true,
+    --         },
+    --       },
+    --     },
+    --   },
+    --   keys = {
+    --     { '<leader>omm', '<Cmd>Markview Toggle<Cr>', { desc = 'Toggle Markview' } },
+    --     { '<leader>omh', '<Cmd>Markview hybridToggle<Cr>', { desc = 'Toggle Hybrid Mode' } },
+    --     { '<leader>omp', '<Cmd>Markview splitToggle<Cr>', { desc = 'Toggle Split View' } },
+    --   },
+    -- },
 
     --  Harpoon
     {
@@ -2219,37 +2219,37 @@ require('lazy').setup(
       end,
     },
 
-    -- snaks image
-    {
-      'folke/snacks.nvim',
-      ---@type snacks.Config
-      opts = {
-        image = {
-          -- your image configuration comes here
-          -- or leave it empty to use the default settings
-          -- refer to the configuration section below
-        },
-      },
-    },
-
-    -- img-clip
-    {
-      'HakonHarnes/img-clip.nvim',
-      event = 'VeryLazy',
-      opts = {
-        -- add options here
-        -- or leave it empty to use the default settings
-        default = {
-          -- file and directory options
-          dir_path = 'assets', ---@type string | fun(): string
-          use_absolute_path = false, ---@type boolean | fun(): boolean
-        },
-      },
-      keys = {
-        -- suggested keymap
-        { '<leader>op', '<cmd>PasteImage<cr>', desc = 'Paste image from system clipboard' },
-      },
-    },
+    -- -- snaks image
+    -- {
+    --   'folke/snacks.nvim',
+    --   ---@type snacks.Config
+    --   opts = {
+    --     image = {
+    --       -- your image configuration comes here
+    --       -- or leave it empty to use the default settings
+    --       -- refer to the configuration section below
+    --     },
+    --   },
+    -- },
+    --
+    -- -- img-clip
+    -- {
+    --   'HakonHarnes/img-clip.nvim',
+    --   event = 'VeryLazy',
+    --   opts = {
+    --     -- add options here
+    --     -- or leave it empty to use the default settings
+    --     default = {
+    --       -- file and directory options
+    --       dir_path = 'assets', ---@type string | fun(): string
+    --       use_absolute_path = false, ---@type boolean | fun(): boolean
+    --     },
+    --   },
+    --   keys = {
+    --     -- suggested keymap
+    --     { '<leader>op', '<cmd>PasteImage<cr>', desc = 'Paste image from system clipboard' },
+    --   },
+    -- },
 
     -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
     -- init.lua. If you want these files, they are in the repository, so you can just download them and
