d34144f1b4573227bae736519441cd0dc5887104
Luiz Gustavo <luizgmdcm@gmail.com>
2025-04-27 18:38:06 -0300

    MC: Add unsed winbar config

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -4,7 +4,7 @@
 
 vim.g.snacks_animate = false
 
--- vim.opt.scrolloff = 28
+-- vim.opt.scrolloff = 999
 vim.opt.scrolloff = 0
 
 vim.o.wrap = false
@@ -12,6 +12,20 @@ vim.o.wrap = false
 vim.o.list = false
 
 vim.opt.spelllang = { "pt_br", "en_us", "es" }
+
+-- local function Winbar()
+--   local normal_color = "%#Normal#"
+--   local mode = "%-5{%v:lua.string.upper(v:lua.vim.fn.mode())%}"
+--   local file_name = "%-.16t"
+--   local buf_nr = "[%n]"
+--   local modified = "%#MiniIconsRed% %-M"
+--   local file_type = " %y"
+--   local right_align = "%="
+--   local line_no = "%10([%l/%L%)]"
+--   local pct_thru_file = "%5p%%"
+--   return string.format("%s%s%s", normal_color, file_name, modified)
+-- end
+-- vim.opt.winbar = Winbar()
 
 ----------------------
 -- NEOVIDE          --
