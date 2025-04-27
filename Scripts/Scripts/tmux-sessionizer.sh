a0b51182ecd21062254d8d6e7b79724a5712f5a0
Luiz Gustavo <luizgmdcm@gmail.com>
2025-04-27 18:36:49 -0300

    CH: Change sessoionizer to work with zoxide

--- Scripts/Scripts/tmux-sessionizer.sh
+++ Scripts/Scripts/tmux-sessionizer.sh
@@ -3,9 +3,10 @@
 if [[ $# -eq 1 ]]; then
   selected=$1
 else
-  selected=$(zoxide query -l -s | fzf)
-  # selected=$(fd --hidden -E .git -E .cache -E .astah -E Documents/ -E .cargo -E .local -E .docker -E .fzf -E Downloads -E Documents -E Pictures --type d --search-path ~/.config/ | fzf)
+  selected=$(zoxide query -l -s | fzf --preview "exa --tree --level=1 \$(echo {} | awk '{print \$2}')")
 fi
+
+selected_path=$(echo $selected | awk '{print $2}')
 
 if [[ -z $selected ]]; then
   exit 0
@@ -15,12 +16,12 @@ selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-  tmux new-session -s $selected_name -c $selected
+  tmux new-session -s $selected_name -c $selected_path
   exit 0
 fi
 
 if ! tmux has-session -t=$selected_name 2>/dev/null; then
-  tmux new-session -ds $selected_name -c $selected
+  tmux new-session -ds $selected_name -c $selected_path
 fi
 
 tmux switch-client -t $selected_name
