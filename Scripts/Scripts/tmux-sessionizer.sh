ad3f4c9b1a9fab316aa8d35d71e6c0bce0f97972
Luiz Gustavo <luizgmdcm@gmail.com>
2025-04-26 23:05:20 -0300

    CH: Change find metodh to zoxide query list and remove commentarys

--- Scripts/Scripts/tmux-sessionizer.sh
+++ Scripts/Scripts/tmux-sessionizer.sh
@@ -3,12 +3,8 @@
 if [[ $# -eq 1 ]]; then
   selected=$1
 else
-  selected=$(fd --hidden -E .git -E .cache -E .astah -E Documents/ -E .cargo -E .local -E .docker -E .fzf -E Downloads -E Documents -E Pictures --type d --search-path ~/.config/ | fzf)
-  # selected=$(fd --hidden -E .git --type d --max-depth=2 --search-path ~/Faculdade/ --search-path ~/dotfiles/ --search-path ~/Projetos/ | fzf)
-  # selected=$(find ~/Faculdade/ ~/dotfiles/ ~/Projetos/ ~/ -mindepth 1 -maxdepth 2 -type d | fzf)
-  # selected=$(fd --hidden --type d | fzf)
-  #
-# ⚡ fd --search-path ~/ -E={.git,.fzf,TMP,go,.docker,Pictures,Downloads,Documents,snap,Music,Desktop,StirlingPDF} --type d --max-depth=2                   <<<
+  selected=$(zoxide query -l -s | fzf)
+  # selected=$(fd --hidden -E .git -E .cache -E .astah -E Documents/ -E .cargo -E .local -E .docker -E .fzf -E Downloads -E Documents -E Pictures --type d --search-path ~/.config/ | fzf)
 fi
 
 if [[ -z $selected ]]; then
