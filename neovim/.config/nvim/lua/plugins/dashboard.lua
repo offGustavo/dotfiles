fdc683165fe64a10a4b54daf08ff1208da35cac9
offGustavo <luizgmdcm@gmail.com>
2025-05-01 19:21:28 -0300

    ADD: add if statement if run in arch

--- neovim/.config/nvim/lua/plugins/dashboard.lua
+++ neovim/.config/nvim/lua/plugins/dashboard.lua
@@ -1,12 +1,22 @@
-return {
-  "snacks.nvim",
-  opts = {
-    dashboard = {
-      preset = {
-        pick = function(cmd, opts)
-          return LazyVim.pick(cmd, opts)()
-        end,
-        header = [[
+-- Executa o comando para obter o ID da distro
+local handle = io.popen("grep '^ID=' /etc/os-release | cut -d'=' -f2")
+local distro = handle:read("*a")
+handle:close()
+
+-- Remove espaços em branco extras
+distro = distro:gsub("%s+", "")
+
+-- Verifica se é Arch Linux e executa o comando
+if distro == "arch" then
+  return {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          pick = function(cmd, opts)
+            return LazyVim.pick(cmd, opts)()
+          end,
+          header = [[
  ███                                           █████         
 ░███                                          ░░███          
 ░███ █████ █████  █████   ██████   ██████   ███████   ██████
@@ -16,68 +26,153 @@ return {
  ███  ░░█████    ██████ ░░██████ ░░██████ ░░████████░░██████
 ░░░    ░░░░░    ░░░░░░   ░░░░░░   ░░░░░░   ░░░░░░░░  ░░░░░░
             ]],
-        -- stylua: ignore
-        ---@type snacks.dashboard.Item[]
-        keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = " ", key = "z", desc = "Change Directory", action = ":lua Snacks.dashboard.pick('zoxide')" },
-          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
-          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          -- stylua: ignore
+          ---@type snacks.dashboard.Item[]
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = " ", key = "z", desc = "Change Directory", action = ":lua Snacks.dashboard.pick('zoxide')" },
+            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
         },
-      },
-      sections = {
-        { section = "header" },
-        {
-          pane = 2,
-          section = "startup",
-          padding = { 1, 0 },
-        },
-        {
-          pane = 2,
-          section = "terminal",
-          -- cmd = "pokeget --hide-name bulbasaur",
-          cmd = "colorscript -e square",
-          -- cmd = "colorscript -e pacman",
-          -- cmd = "chafa ~/Pictures/Wallpapers/my/gengara.jpg --format symbols --symbols vhalf --size 60x11 --stretch",
-          -- cmd = "fzf",
-          height = 7,
-          padding = { 2, 0 },
-          -- indent = 10,
-        },
-        -- {
-        --   pane = 2,
-        --   icon = " ",
-        --   desc = "Browse Repo",
-        --   padding = 1,
-        --   key = "b",
-        --   action = function()
-        --     Snacks.gitbrowse()
-        --   end,
-        -- },
-        { section = "keys", gap = 1, padding = 1 },
-        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-        {
-          pane = 2,
-          icon = " ",
-          title = "Git Status",
-          section = "terminal",
-          enabled = function()
-            return Snacks.git.get_root() ~= nil
-          end,
-          cmd = "git status --short --branch --renames",
-          height = 5,
-          padding = 1,
-          ttl = 5 * 60,
-          indent = 3,
+        sections = {
+          { section = "header" },
+          {
+            pane = 2,
+            section = "startup",
+            padding = { 1, 0 },
+          },
+          {
+            pane = 2,
+            section = "terminal",
+            -- cmd = "pokeget --hide-name bulbasaur",
+            cmd = "colorscript -e square",
+            -- cmd = "colorscript -e pacman",
+            -- cmd = "chafa ~/Pictures/Wallpapers/my/gengara.jpg --format symbols --symbols vhalf --size 60x11 --stretch",
+            -- cmd = "fzf",
+            height = 7,
+            padding = { 2, 0 },
+            -- indent = 10,
+          },
+          -- {
+          --   pane = 2,
+          --   icon = " ",
+          --   desc = "Browse Repo",
+          --   padding = 1,
+          --   key = "b",
+          --   action = function()
+          --     Snacks.gitbrowse()
+          --   end,
+          -- },
+          { section = "keys", gap = 1, padding = 1 },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = function()
+              return Snacks.git.get_root() ~= nil
+            end,
+            cmd = "git status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
         },
       },
     },
-  },
-}
+  }
+else
+  return {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          pick = function(cmd, opts)
+            return LazyVim.pick(cmd, opts)()
+          end,
+          header = [[
+ ███                                           █████         
+░███                                          ░░███          
+░███ █████ █████  █████   ██████   ██████   ███████   ██████
+░███░░███ ░░███  ███░░   ███░░███ ███░░███ ███░░███  ███░░███
+░███ ░███  ░███ ░░█████ ░███ ░░░ ░███ ░███░███ ░███ ░███████
+░░░  ░░███ ███   ░░░░███░███  ███░███ ░███░███ ░███ ░███░░░
+ ███  ░░█████    ██████ ░░██████ ░░██████ ░░████████░░██████
+░░░    ░░░░░    ░░░░░░   ░░░░░░   ░░░░░░   ░░░░░░░░  ░░░░░░
+            ]],
+          -- stylua: ignore
+          ---@type snacks.dashboard.Item[]
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = " ", key = "z", desc = "Change Directory", action = ":lua Snacks.dashboard.pick('zoxide')" },
+            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+        },
+        sections = {
+          { section = "header" },
+          {
+            pane = 1,
+            section = "startup",
+            padding = { 1, 0 },
+          },
+          -- {
+          --   pane = 2,
+          --   section = "terminal",
+          --   -- cmd = "pokeget --hide-name bulbasaur",
+          --   cmd = "colorscript -e square",
+          --   -- cmd = "colorscript -e pacman",
+          --   -- cmd = "chafa ~/Pictures/Wallpapers/my/gengara.jpg --format symbols --symbols vhalf --size 60x11 --stretch",
+          --   -- cmd = "fzf",
+          --   height = 7,
+          --   padding = { 2, 0 },
+          --   -- indent = 10,
+          -- },
+          -- {
+          --   pane = 2,
+          --   icon = " ",
+          --   desc = "Browse Repo",
+          --   padding = 1,
+          --   key = "b",
+          --   action = function()
+          --     Snacks.gitbrowse()
+          --   end,
+          -- },
+          { section = "keys", gap = 1, padding = 1 },
+          { pane = 1, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { pane = 1, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          -- {
+          --   pane = 1,
+          --   icon = " ",
+          --   title = "Git Status",
+          --   section = "terminal",
+          --   enabled = function()
+          --     return Snacks.git.get_root() ~= nil
+          --   end,
+          --   cmd = "git status --short --branch --renames",
+          --   height = 5,
+          --   padding = 1,
+          --   ttl = 5 * 60,
+          --   indent = 3,
+          -- },
+        },
+      },
+    },
+  }
+end
