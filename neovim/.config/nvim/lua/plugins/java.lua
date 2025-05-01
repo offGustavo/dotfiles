2e6bed80814b4b11b1fe1a230e523b35f84704f4
Luiz Gustavo <luizgmdcm@gmail.com>
2025-05-01 12:27:26 -0300

    RM: Remove Springboot

--- neovim/.config/nvim/lua/plugins/java.lua
+++ neovim/.config/nvim/lua/plugins/java.lua
@@ -1,11 +1,24 @@
 return {
-  "nvim-java/nvim-java",
-  config = function()
-    require("java").setup()
-    require("lspconfig").jdtls.setup({})
-  end,
-  keys = {
-    { "<leader>cjr", "<Cmd>JavaRunnerRunMain<Cr>", { desc = "Run Main" } },
-    { "<leader>cjs", "<Cmd>JavaRunnerStopMain<Cr>", { desc = "Stop Main" } },
+  {
+    "nvim-java/nvim-java",
+    config = function()
+      require("java").setup()
+      require("lspconfig").jdtls.setup({})
+    end,
+    keys = {
+      { "<leader>cjr", "<Cmd>JavaRunnerRunMain<Cr>", { desc = "Run Main" } },
+      { "<leader>cjs", "<Cmd>JavaRunnerStopMain<Cr>", { desc = "Stop Main" } },
+    },
   },
+  -- {
+  --   "JavaHello/spring-boot.nvim",
+  --   ft = "java",
+  --   dependencies = {
+  --     "mfussenegger/nvim-jdtls", -- or nvim-java, nvim-lspconfig
+  --     "ibhagwan/fzf-lua", -- optional
+  --   },
+  --   config = function()
+  --     require("spring_boot").setup({})
+  --   end,
+  -- },
 }
