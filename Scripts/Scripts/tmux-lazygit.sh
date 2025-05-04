f3c3c6fa0e24cca4e65eba1d6aa3ff5256fc0003
Luiz Gustavo <luizgmdcm@gmail.com>
2025-05-03 21:31:32 -0300

    CH: Change script and make variable

--- Scripts/Scripts/tmux-lazygit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if tmux list-windows -F "#{window_name}" | grep -q "lazygit"; then
-  tmux select-window -t lazygit
-else
-  tmux new-window -n lazygit -c "#{pane_current_path}" lazygit
-fi
