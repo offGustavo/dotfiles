ab062219201e04c2a500fe7c28d6b6ec52ba7f7f
Luiz Gustavo <luizgmdcm@gmail.com>
2025-05-06 00:31:10 -0300

    ADD: add source file keymap

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -99,6 +99,7 @@ vim.keymap.set("x", "<leader>p", '"_dP', { desc = "Paste Without Copy" })
 vim.keymap.set("n", "<leader>fx", ":!chmod +x %<Cr>", { desc = "Make File Executable" })
 
 vim.keymap.set("n", "<leader>fX", ":!%<Cr>", { desc = "Execute File" })
+vim.keymap.set("n", "<leader>fs", ":so %<Cr>", { desc = "Source File" })
 
 vim.keymap.set("n", "<leader>m", "`", { desc = "Go to Marks" })
 
