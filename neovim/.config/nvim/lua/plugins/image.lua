1f28b82d383f8348440b0e55743f0ee096d1cacd
Luiz Gustavo <luizgmdcm@gmail.com>
2025-05-10 18:03:21 -0300

    CH: change image clip keymap

--- neovim/.config/nvim/lua/plugins/image.lua
+++ neovim/.config/nvim/lua/plugins/image.lua
@@ -24,7 +24,7 @@ return {
     },
     keys = {
       -- suggested keymap
-      { "<leader>oop", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+      { "<leader>op", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
     },
   },
 }
