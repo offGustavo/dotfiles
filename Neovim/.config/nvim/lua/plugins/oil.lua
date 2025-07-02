6ef5a07547dd6a7c2f852cc298d597bd4126b5be
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-07-02 11:49:00 -0300

    ADD: add toggle file detail keymap

--- Neovim/.config/nvim/lua/plugins/oil.lua
+++ Neovim/.config/nvim/lua/plugins/oil.lua
@@ -83,6 +83,17 @@ return {
         ["gx"] = "actions.open_external",
         ["g."] = { "actions.toggle_hidden", mode = "n" },
         ["g\\"] = { "actions.toggle_trash", mode = "n" },
+        ["gd"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            detail = not detail
+            if detail then
+              require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+            else
+              require("oil").set_columns({ "icon" })
+            end
+          end,
+        },
       },
       -- Set to false to disable all of the above keymaps
       use_default_keymaps = true,
