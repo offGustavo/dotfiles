b8b7a4788ae8f2281be1ae20419b1f8e849407be
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-07-07 03:35:25 -0300

    ADD: add undo and save and quit keymaps

--- Neovim/.config/nvim/lua/config/keymaps.lua
+++ Neovim/.config/nvim/lua/config/keymaps.lua
@@ -82,7 +82,9 @@ vim.keymap.set("i", "<A-}>", "<C-o>}", { silent = true })
 vim.keymap.set("i", "<A-{>", "<C-o>{", { silent = true })
 vim.keymap.set("i", "<A-<>", "<C-o>gg", { silent = true })
 vim.keymap.set("i", "<A->>", "<C-o>G", { silent = true })
+vim.keymap.set("i", "<C-/>", "<C-o>u", { silent = true })
 vim.keymap.set("i", "<C-x><C-s>", "<C-o>:w<CR>", { silent = true })
+vim.keymap.set("i", "<C-x><C-c>", "<C-o>:wq<CR>", { silent = true })
 
 -- Normal mode in command line
 vim.keymap.set("c", "<C-o>", "<C-f>", { silent = true })
