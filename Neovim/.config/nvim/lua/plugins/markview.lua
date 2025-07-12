0b48d6d744190909c5aae83b73fe52c61ec50e73
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-07-12 15:06:52 -0300

    FIX: fix markview update

--- Neovim/.config/nvim/lua/plugins/markview.lua
+++ Neovim/.config/nvim/lua/plugins/markview.lua
@@ -7,37 +7,40 @@ return {
       hybrid_modes = { "n" },
       linewise_hybrid_mode = true,
     },
+
     -- list_items
-    list_items = {
-      enable = true,
-      wrap = false,
-      indent_size = 2,
-      shift_width = 4,
-      marker_minus = {
-        add_padding = true,
-        conceal_on_checkboxes = true,
-        text = "",
-        hl = "MarkviewListItemMinus",
-      },
-      marker_plus = {
-        add_padding = true,
-        conceal_on_checkboxes = true,
-        text = "",
-        hl = "MarkviewListItemPlus",
-      },
-      marker_star = {
-        add_padding = true,
-        conceal_on_checkboxes = true,
-        text = "",
-        hl = "MarkviewListItemStar",
-      },
-      marker_dot = {
-        add_padding = true,
-        conceal_on_checkboxes = true,
-      },
-      marker_parenthesis = {
-        add_padding = true,
-        conceal_on_checkboxes = true,
+    markdown = {
+      list_items = {
+        enable = true,
+        wrap = false,
+        indent_size = 2,
+        shift_width = 4,
+        marker_minus = {
+          add_padding = true,
+          conceal_on_checkboxes = true,
+          text = "",
+          hl = "MarkviewListItemMinus",
+        },
+        marker_plus = {
+          add_padding = true,
+          conceal_on_checkboxes = true,
+          text = "",
+          hl = "MarkviewListItemPlus",
+        },
+        marker_star = {
+          add_padding = true,
+          conceal_on_checkboxes = true,
+          text = "",
+          hl = "MarkviewListItemStar",
+        },
+        marker_dot = {
+          add_padding = true,
+          conceal_on_checkboxes = true,
+        },
+        marker_parenthesis = {
+          add_padding = true,
+          conceal_on_checkboxes = true,
+        },
       },
     },
   },
