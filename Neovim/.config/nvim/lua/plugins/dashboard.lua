9fc3b09d53babe1c8174d37394c2a9e987985bf6
offGustavo <125596752+offGustavo@users.noreply.github.com>
2025-07-23 17:09:05 -0300

    FIX: disable dashboard when colorscript is not available

--- Neovim/.config/nvim/lua/plugins/dashboard.lua
+++ Neovim/.config/nvim/lua/plugins/dashboard.lua
@@ -1,12 +1,22 @@
-return {
-  "snacks.nvim",
-  opts = {
-    dashboard = {
-      preset = {
-        pick = function(cmd, opts)
-          return LazyVim.pick(cmd, opts)()
-        end,
-        header = [[
+if vim.fn.executable("colorscript") == 0 then
+  return {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        enabled = false,
+      },
+    },
+  }
+else
+  return {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          pick = function(cmd, opts)
+            return LazyVim.pick(cmd, opts)()
+          end,
+          header = [[
  ███                                           █████         
 ░███                                          ░░███          
 ░███ █████ █████  █████   ██████   ██████   ███████   ██████
@@ -30,54 +40,55 @@ return {
             { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
             { icon = " ", key = "q", desc = "Quit", action = ":qa" },
           },
-      },
-      sections = {
-        { section = "header" },
-        {
-          pane = 2,
-          section = "startup",
-          padding = { 1, 0 },
         },
-        {
-          pane = 2,
-          section = "terminal",
-          -- cmd = "pokeget --hide-name bulbasaur",
-          cmd = "colorscript -e square",
-          -- cmd = "colorscript -e pacman",
-          -- cmd = "chafa ~/Pictures/Wallpapers/my/gengara.jpg --format symbols --symbols vhalf --size 60x11 --stretch",
-          -- cmd = "fzf",
-          height = 7,
-          padding = { 2, 0 },
-          -- indent = 10,
-        },
-        {
-          pane = 2,
-          icon = " ",
-          desc = "Browse Repo",
-          padding = 1,
-          key = "b",
-          action = function()
-            Snacks.gitbrowse()
-          end,
-        },
-        { section = "keys", gap = 1, padding = 1 },
-        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-        {
-          pane = 2,
-          icon = " ",
-          title = "Git Status",
-          section = "terminal",
-          enabled = function()
-            return Snacks.git.get_root() ~= nil
-          end,
-          cmd = "git status --short --branch --renames",
-          height = 5,
-          padding = 1,
-          ttl = 5 * 60,
-          indent = 3,
+        sections = {
+          { section = "header" },
+          {
+            pane = 2,
+            section = "startup",
+            padding = { 1, 0 },
+          },
+          {
+            pane = 2,
+            section = "terminal",
+            -- cmd = "pokeget --hide-name bulbasaur",
+            cmd = "colorscript -e square",
+            -- cmd = "colorscript -e pacman",
+            -- cmd = "chafa ~/Pictures/Wallpapers/my/gengara.jpg --format symbols --symbols vhalf --size 60x11 --stretch",
+            -- cmd = "fzf",
+            height = 7,
+            padding = { 2, 0 },
+            -- indent = 10,
+          },
+          {
+            pane = 2,
+            icon = " ",
+            desc = "Browse Repo",
+            padding = 1,
+            key = "b",
+            action = function()
+              Snacks.gitbrowse()
+            end,
+          },
+          { section = "keys", gap = 1, padding = 1 },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = function()
+              return Snacks.git.get_root() ~= nil
+            end,
+            cmd = "git status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
         },
       },
     },
-  },
-}
+  }
+end
