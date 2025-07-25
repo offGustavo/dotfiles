244d4c424b4a03aafb2e7fc31f969b33312e8b53
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-07-25 15:36:07 -0300

    CH: disable screenken

--- Neovim/.config/nvim/lua/plugins/screenkey.lua
+++ Neovim/.config/nvim/lua/plugins/screenkey.lua
@@ -1,6 +1,7 @@
 return {
   "NStefan002/screenkey.nvim",
   lazy = true,
+  enabled = false,
   version = "*", -- or branch = "main", to use the latest commit
   keys = {
     { "<leader>os", "<Cmd>Screenkey<Cr>", { desc = "Screenkey Toggle" } },
