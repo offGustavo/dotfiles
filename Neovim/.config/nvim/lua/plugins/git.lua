b6b033ae909f461a82ab3c7db9a335295600acba
offGustavo <125596752+offGustavo@users.noreply.github.com>
2025-07-27 19:12:42 -0300

    MC: add silent to fugitive keymap

--- Neovim/.config/nvim/lua/plugins/git.lua
+++ Neovim/.config/nvim/lua/plugins/git.lua
@@ -1,19 +1,20 @@
 return {
-  { "tpope/vim-fugitive", 
-    config = function ()
-      vim.keymap.set("n", '<leader>G', ":Git<Cr>:only<Cr>", { desc = "Fugitive"})
-    end
+  {
+    "tpope/vim-fugitive",
+    config = function()
+      vim.keymap.set("n", "<leader>G", ":Git<Cr>:only<Cr>", { desc = "Fugitive", silent = true })
+    end,
   },
   {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim", -- required
-    "sindrets/diffview.nvim", -- optional - Diff integration
-    "folke/snacks.nvim", -- optional
-  },
-  config = function()
-    vim.keymap.set("n", "<leader>gn", ":Neogit kind=floating cwd=%:h<Cr>", { silent = true, desc = "Neogit Float" })
-    vim.keymap.set("n", "<leader>gN", ":Neogit kind=replace cwd=%:h<Cr>", { silent = true, desc = "Neogit" })
-  end,
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+      "folke/snacks.nvim", -- optional
+    },
+    config = function()
+      vim.keymap.set("n", "<leader>gn", ":Neogit kind=floating cwd=%:h<Cr>", { silent = true, desc = "Neogit Float" })
+      vim.keymap.set("n", "<leader>gN", ":Neogit kind=replace cwd=%:h<Cr>", { silent = true, desc = "Neogit" })
+    end,
   }
 }
