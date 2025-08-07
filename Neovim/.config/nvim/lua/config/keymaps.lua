899ac02d8aaa1fd1a3e12d84315990fa1a75d0bf
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-08-07 16:32:24 -0300

    CH: add some keymaps to smart picker

--- Neovim/.config/nvim/lua/config/keymaps.lua
+++ Neovim/.config/nvim/lua/config/keymaps.lua
@@ -403,6 +403,7 @@ else
 
   vim.keymap.set("n", "<leader><space>", function()
     Snacks.picker.smart({
+      -- finder = "explorer",
       hidden = true,
       matcher = {
         frequency = true,
@@ -415,6 +416,34 @@ else
             ["K"] = "preview_scroll_up",
             ["H"] = "preview_scroll_left",
             ["L"] = "preview_scroll_right",
+            ["<BS>"] = "explorer_up",
+            ["l"] = "confirm",
+            ["h"] = "explorer_close", -- close directory
+            ["a"] = "explorer_add",
+            ["D"] = "explorer_del",
+            ["r"] = "explorer_rename",
+            ["c"] = "explorer_copy",
+            ["m"] = "explorer_move",
+            ["o"] = "explorer_open", -- open with system application
+            ["P"] = "toggle_preview",
+            ["y"] = { "explorer_yank", mode = { "n", "x" } },
+            ["p"] = "explorer_paste",
+            ["u"] = "explorer_update",
+            ["<c-c>"] = "tcd",
+            ["<leader>/"] = "picker_grep",
+            ["<c-t>"] = "terminal",
+            ["."] = "explorer_focus",
+            ["I"] = "toggle_ignored",
+            -- ["H"] = "toggle_hidden",
+            -- ["Z"] = "explorer_close_all",
+            ["]g"] = "explorer_git_next",
+            ["[g"] = "explorer_git_prev",
+            ["]d"] = "explorer_diagnostic_next",
+            ["[d"] = "explorer_diagnostic_prev",
+            ["]w"] = "explorer_warn_next",
+            ["[w"] = "explorer_warn_prev",
+            ["]e"] = "explorer_error_next",
+            ["[e"] = "explorer_error_prev",
           },
         },
         list = {
