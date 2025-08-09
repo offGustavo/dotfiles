c790dfcd146d8870b0c7ed408b7bbaa236ab4d00
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-08-09 15:53:35 -0300

    CH: fix comment syntax in custom filetype

--- Neovim/.config/nvim/ftplugin/kbd.lua
+++ Neovim/.config/nvim/ftplugin/kbd.lua
@@ -1,7 +1,21 @@
-vim.bo.commentstring = ";;%s"
-vim.wo.foldmethod = "indent"
--- Ensure syntax highlighting is enabled
-vim.cmd("syntax enable")
+vim.cmd([[
+  " Ensure syntax highlighting is enabled
+  syntax enable
+  " Force load Lisp syntax rules
+  runtime! syntax/lisp.vim
+  " Remove the old comment rule
+  syntax clear lispComment
+  " Match only ;; comments
+  syntax match lispComment ";;.*" contains=@Spell
+  " Link to Comment highlight
+  " Ensure lispComment is allowed inside all lisp regions
+  highlight def link lispComment Comment
+  " (including function bodies, lists, etc.)
+  syntax cluster lispTop add=lispComment
+  syntax cluster lispExpr add=lispComment
+]])
 
--- Force load Lisp syntax rules
-vim.cmd("runtime! syntax/lisp.vim")
+-- Set Comments
+vim.bo.commentstring = ";; %s"
+
+vim.wo.foldmethod = "indent"
