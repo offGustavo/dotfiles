8aa3272129879d0bc321d8407d2bb9de55c843dd
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-08-21 23:50:57 -0300

    FIX: fix mode

--- Neovim/.config/nvim/lua/config/keymaps.lua
+++ Neovim/.config/nvim/lua/config/keymaps.lua
@@ -6,8 +6,8 @@ vim.keymap.set({ "n", "v" }, "n", "nzz", { silent = true, desc = "Next Search Re
 vim.keymap.set("n", "N", "Nzz", { silent = true, desc = "Previous Search Result" })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true, desc = "Half Scroll Down and Center" })
 vim.keymap.set("n", "<C-u>", "<c-u>zz", { silent = true, desc = "Half Scroll Up and Center" })
-vim.keymap.set("n", ">", ">gv", { silent = true, desc = "Better Indent" })
-vim.keymap.set("n", "<", "<gv", { silent = true, desc = "Better Indent" })
+vim.keymap.set("v", ">", ">gv", { silent = true, desc = "Better Indent" })
+vim.keymap.set("v", "<", "<gv", { silent = true, desc = "Better Indent" })
 vim.keymap.set("n", "gJ", "kJ_", { silent = true, desc = "Join Line Above" })
 vim.keymap.set("n", "J", "J_", { silent = true, desc = "Better Join Line" })
 
