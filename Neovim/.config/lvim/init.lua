2283bf41f18f6c49107e88ea66f4aab1a8bcb4af
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-09-02 11:43:14 -0300

    CH: no plugin?

--- Neovim/.config/lvim/init.lua
+++ Neovim/.config/lvim/init.lua
@@ -30,23 +30,8 @@ require('config.autocmds')
 require("plugins.theme")
 require("plugins.mason-lsp-config")
 require("plugins.treesitter")
-require("plugins.neogit")
+-- require("plugins.neogit")
 -- require("plugins.mini")
 -- require("plugins.snacks")
 -- require("plugins.oil")
 
-
-vim.pack.add({ 'https://github.com/dmtrKovalenko/fff.nvim' })
-
--- the plugin will automatically lazy load
-vim.g.fff = {
-  lazy_sync = true, -- start syncing only when the picker is open
-  debug ={
-    enabled = true,
-    show_scores = true,
-  }
-}
-
-vim.keymap.set('n', 'ff', function()
-   require('fff').find_files()
-end, { desc = 'FFFind files' })
