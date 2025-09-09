49869e3cb8f731f06379f913c374223167c05dcb
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-09-09 19:39:18 -0300

    CH: fix alt tab  in i3wm

--- Rofi/.config/rofi/scripts/windows.sh
+++ Rofi/.config/rofi/scripts/windows.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-rofi -show window -kb-accept-entry '!Alt-Tab' -kb-row-down Alt-Tab 
+case "$1" in
+  --alt-tab)
+    rofi -show window -kb-accept-entry '!Alt-Tab' -kb-row-down Alt-Tab
+    ;;
+  *)
+    rofi -show window
+    ;;
+esac
