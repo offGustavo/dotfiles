73a728982c8809286db5b41a3ffdbdb44ed3e8f2
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-09-22 18:38:42 -0300

    feat(snacks picker): remove preview by default in ivy_split

--- Neovim/.config/nvim/lua/plugins/snacks.lua
+++ Neovim/.config/nvim/lua/plugins/snacks.lua
@@ -5,21 +5,45 @@ return {
     -- scope = { enabled = false },
     explorer = {
       replace_netrw = false,
-      layout = "ivy",
     },
     input = { enabled = false },
     picker = {
       -- layout = "ivy",
       -- layout = "telescope",
-      layout = "ivy_split",
+      -- layout = "ivy_split",
+        layout = {
+          preset = "ivy_split",
+          hidden = { "preview" }, -- Isso esconde o preview inicialmente
+        },
+        preview = "main", -- Garante que o preview vai para a main window
+      -- layout = {
+      --   preview = false,
+      --   layout = {
+      --     box = "vertical",
+      --     backdrop = false,
+      --     width = 0,
+      --     height = 0.4,
+      --     position = "bottom",
+      --     border = "top",
+      --     title = " {title} {live} {flags}",
+      --     title_pos = "left",
+      --     { win = "input", height = 1, border = "none" },
+      --     {
+      --       box = "horizontal",
+      --       { win = "list", border = "none" },
+      --       { win = "preview", title = "{preview}", width = 0.6, border = "rounded" },
+      --     },
+      --   },
+      -- },
+      -- ui_select = false,
     },
     animate = {
       duration = 10, -- ms per step
       easing = "linear",
       fps = 60, -- frames per second. Global setting for all animations
     },
-    scope = {
-      enabled = false
-    },
+    -- scope = {
+    -- enabled = false,
+    -- },
   },
 }
