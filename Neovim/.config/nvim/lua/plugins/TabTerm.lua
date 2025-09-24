b097c259ee5b18453e156eeedc9753d7f72270a4
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-09-24 11:26:32 -0300

    chore(tabterm): remove cmd fild and format code

--- Neovim/.config/nvim/lua/plugins/TabTerm.lua
+++ Neovim/.config/nvim/lua/plugins/TabTerm.lua
@@ -1,156 +1,131 @@
 return {
-  dir = "~/Projects/TabTerm.nvim/",
-  dev = true,
   'offGustavo/TabTerm.nvim',
-  -- enabled = false,
   lazy = true,
-  cmd = {"TabTermNew","TabTermToggle"},
   opts = {
     separator_first = "█",
     separator_right = "",
     separator_left = "",
-  -- separator_right = "",
-  -- separator_left = "",
-  -- separator_first = "",
-  -- separator_first = "",
-  -- separator_first = "",
-  -- separator_first = " ",
-  -- separator_right = "",
-  -- separator_left = "",
-  -- separator_first = "",
-  -- tab_highlight = "%#Tabline#",
-  -- default_highlight = "%#TablineSel#",
+    -- separator_right = "",
+    -- separator_left = "",
+    -- separator_first = "",
+    -- separator_first = "",
+    -- separator_first = "",
+    -- separator_first = " ",
+    -- separator_right = "",
+    -- separator_left = "",
+    -- separator_first = "",
+    -- tab_highlight = "%#Tabline#",
+    -- default_highlight = "%#TablineSel#",
   },
   keys = function()
-
     local keys = {
-
       {
-        mode ={ 'i', 'n', 't'},
+        mode = { "i", "n", "t" },
         "<A-m>c",
-        function ()
-          require('TabTerm').new()
+        function()
+          require("TabTerm").new()
         end,
-        desc = "Create Terminal"
+        desc = "Create Terminal",
       },
-
       {
-        mode ={ 'i', 'n', 't'},
+        mode = { "i", "n", "t" },
         "<A-m>x",
-        function ()
-          require('TabTerm').close()
+        function()
+          require("TabTerm").close()
         end,
-        desc = "Close Terminal"
+        desc = "Close Terminal",
       },
-
       {
-        mode ={ 'i', 'n', 't'},
+        mode = { "i", "n", "t" },
         "<A-m>d",
-        function ()
-          require('TabTerm').toggle()
+        function()
+          require("TabTerm").toggle()
         end,
-        desc = "Toggle Terminal"
+        desc = "Toggle Terminal",
       },
-
       {
-        mode ={ 'i', 'n', 't'},
+        mode = { "i", "n", "t" },
         "<A-m>,",
-        function ()
-          require('TabTerm').rename()
+        function()
+          require("TabTerm").rename()
         end,
-        desc = "Rename Terminal"
+        desc = "Rename Terminal",
       },
-
-
       {
-        mode ={ 'i', 'n', 't'},
+        mode = { "i", "n", "t" },
         "<A-n>",
-        function ()
-          require('TabTerm').new()
+        function()
+          require("TabTerm").new()
         end,
-        desc = "Create Terminal"
+        desc = "Create Terminal",
       },
-
       {
-        mode ={ 'i', 'n', 't'},
+        mode = { "i", "n", "t" },
         "<A-x>",
-        function ()
-          require('TabTerm').close()
+        function()
+          require("TabTerm").close()
         end,
-        desc = "Close Terminal"
+        desc = "Close Terminal",
       },
-
       {
-        mode ={ 'i',
-          'n',
-          't'},
+        mode = { "i", "n", "t" },
         "<A-/>",
-        function ()
-          require('TabTerm').toggle()
+        function()
+          require("TabTerm").toggle()
         end,
-        desc = "Toggle Terminal"
+        desc = "Toggle Terminal",
       },
-
       {
-        mode ={ 'i', 'n', 't'},
+        mode = { "i", "n", "t" },
         "<A-,>",
-        function ()
-          require('TabTerm').rename()
+        function()
+          require("TabTerm").rename()
         end,
-        desc = "Rename Terminal"
+        desc = "Rename Terminal",
       },
-
       {
-        mode ={ 'i', 'n', 't'},
+        mode = { "i", "n", "t" },
         "<A-m>!",
-        function ()
+        function()
           print("Test Legal Apenas")
         end,
-        desc = "which_key_ignore"
+        desc = "which_key_ignore",
       },
-
       {
-        mode ={ 'i', 'n', 't'},
+        mode = { "i", "n", "t" },
         "<A-m>%",
-        function ()
+        function()
           print("Test Legal Apenas")
         end,
-        desc = "which_key_ignore"
+        desc = "which_key_ignore",
       },
-
       {
-        mode ={ 'i', 'n', 't'},
-        "<A-m>\"",
-        function ()
+        mode = { "i", "n", "t" },
+        '<A-m>"',
+        function()
           print("Test Legal Apenas")
         end,
-        desc = "which_key_ignore"
+        desc = "which_key_ignore",
       },
-
-
-
     }
-
     for i = 1, 9 do
       table.insert(keys, {
-        mode ={ 'i', 'n', 't'},
+        mode = { "i", "n", "t" },
         "<A-m>" .. i,
         function()
-          require('TabTerm').go(i)
+          require("TabTerm").go(i)
         end,
-        desc = "which_key_ignore"
+        desc = "which_key_ignore",
       })
       table.insert(keys, {
-        mode ={ 'i', 'n', 't'},
+        mode = { "i", "n", "t" },
         "<A-" .. i .. ">",
         function()
-          require('TabTerm').go(i)
+          require("TabTerm").go(i)
         end,
-        desc = "which_key_ignore"
+        desc = "which_key_ignore",
       })
-
     end
     return keys
-
   end,
 }
