fadc950ef36506a9dcac5c0c4b080d419a6cdeb7
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-09-24 14:51:20 -0300

    chore(tmux sessionizer): format code

--- Scripts/scripts/tmux-sessionizer.sh
+++ Scripts/scripts/tmux-sessionizer.sh
@@ -7,7 +7,6 @@ else
   selected=$(echo "$selected" | awk '{print $2}')
 fi
 
-
 if [[ -z "$selected" ]]; then
   echo "No path!"
   exit 0
@@ -18,17 +17,17 @@ tmux_running=$(pgrep tmux)
 
 if [[ -z "$TMUX" ]] && [[ -z "$tmux_running" ]]; then
   tmux new-session -s "$selected_name" -c "$selected" -e "NVIM_IN_TMUX=1" "nvim"
-    exit 0
+  exit 0
 fi
 
 if [[ -n "$tmux_running" ]]; then
-    if ! tmux has-session -t="$selected_name" 2>/dev/null; then
-        tmux new-session -ds "$selected_name" -c "$selected" -e "NVIM_IN_TMUX=1" "nvim"
-    fi
+  if ! tmux has-session -t="$selected_name" 2>/dev/null; then
+    tmux new-session -ds "$selected_name" -c "$selected" -e "NVIM_IN_TMUX=1" "nvim"
+  fi
 
-    if [[ -n "$TMUX" ]]; then
-        tmux switch-client -t "$selected_name"
-    else
-        tmux attach-session -t "$selected_name"
-    fi
+  if [[ -n "$TMUX" ]]; then
+    tmux switch-client -t "$selected_name"
+  else
+    tmux attach-session -t "$selected_name"
+  fi
 fi
