f068cb6f3b784f143dd1f19cc7bec7696f1d8878
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-10-08 12:36:57 -0300

    feat(neovim dashbaord): add git browse repo

--- Neovim/.config/nvim/lua/plugins/dashboard.lua
+++ Neovim/.config/nvim/lua/plugins/dashboard.lua
@@ -61,16 +61,16 @@ else
             padding = { 2, 0 },
             -- indent = 10,
           },
-          -- {
-          --   pane = 2,
-          --   icon = " ",
-          --   desc = "Browse Repo",
-          --   padding = 1,
-          --   key = "b",
-          --   action = function()
-          --     Snacks.gitbrowse()
-          --   end,
-          -- },
+          {
+            pane = 2,
+            icon = " ",
+            desc = "Browse Repo",
+            padding = 1,
+            key = "b",
+            action = function()
+              Snacks.gitbrowse()
+            end,
+          },
           { section = "keys", gap = 1, padding = 1 },
           { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
           { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
