43cd4faa4d133a9dec60cb6e1290dacf04b61073
offGustavo <125596752+offGustavo@users.noreply.github.com>
2025-10-10 14:29:07 -0300

    feat(neovim): disable undrotree plugin

--- Neovim/.config/nvim/lua/plugins/undotree.lua
+++ Neovim/.config/nvim/lua/plugins/undotree.lua
@@ -1,10 +1,11 @@
-return {
-  "mbbill/undotree",
-  config = function()
-    vim.keymap.set("n", "<leader>ou", vim.cmd.UndotreeToggle, { desc = "Undo Tree Toggle" })
-    vim.cmd([[
-    let g:undotree_WindowLayout = 2
-    let g:undotree_DiffAutoOpen = 1
-      ]])
-  end,
-}
+return {}
+-- return {
+--   "mbbill/undotree",
+--   config = function()
+--     vim.keymap.set("n", "<leader>ou", vim.cmd.UndotreeToggle, { desc = "Undo Tree Toggle" })
+--     vim.cmd([[
+--     let g:undotree_WindowLayout = 2
+--     let g:undotree_DiffAutoOpen = 1
+--       ]])
+--   end,
+-- }
