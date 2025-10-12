f3c46ee8c1b43224cc0030ba606d5cdb13cb03e2
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-10-12 10:35:38 -0300

    fix(neovim): remove check for windwos

--- Neovim/.config/nvim/init.lua
+++ Neovim/.config/nvim/init.lua
@@ -4,14 +4,14 @@ require("config.lazy")
 -- Custom Things
 require("fish.multi_marks")
 require("fish.commads")
-
-require("fish.neovide")
-require("fish.vscode")
 require("fish.filetypes")
 
-if not vim.uv.os_uname().sysname == "Windows_NT" then
-  require("fish.theme")
-  require("fish.kitty")
-  require("fish.tmux")
-end
+-- External Programs
+require("fish.neovide")
+require("fish.vscode")
+
+-- Theme
+require("fish.theme")
+require("fish.kitty")
+require("fish.tmux")
 
