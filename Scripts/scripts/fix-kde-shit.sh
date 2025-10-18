0275c0a40c04f2572fb4477a1bc789eb9708d793
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-10-17 22:25:51 -0300

    feat(scripts): start gnome keyring

--- Scripts/scripts/fix-kde-shit.sh
+++ Scripts/scripts/fix-kde-shit.sh
@@ -3,3 +3,5 @@
 gsettings reset org.gnome.desktop.interface icon-theme
 
 gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
+
+systemctl --user start gnome-keyring-daemon.service
