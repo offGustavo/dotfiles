a1f6d10e51d0bd2438cc86a7774fb4ab58b06cc4
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-11-30 18:54:19 -0300

    feat(hyprland): add light and dark theme in hyprland and waybar

--- Hyprland/.config/hypr/scripts/toggle-dark-mode.sh
+++ Hyprland/.config/hypr/scripts/toggle-dark-mode.sh
@@ -3,8 +3,14 @@ current_scheme=$(gsettings get org.gnome.desktop.interface color-scheme)
 
 if [ "$current_scheme" = "'prefer-light'" ] || [ "$current_scheme" = "'default'" ]; then
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-    notify-send "Dark Mode" "Switched to dark mode"
+    killall waybar
+    waybar -c ~/.config/hypr/waybar/config.jsonc -s ~/.config/hypr/waybar/style.css
+    ln -sf ~/.config/fuzzel/light.ini ~/.config/fuzzel/fuzzel.ini
+    # notify-send "Dark Mode" "Switched to dark mode"
 else
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
-    notify-send "Light Mode" "Switched to light mode"
+    killall waybar
+    waybar -c ~/.config/hypr/waybar/config.jsonc -s ~/.config/hypr/waybar/light.css
+    ln -sf ~/.config/fuzzel/dark.ini ~/.config/fuzzel/fuzzel.ini
+    # notify-send "Light Mode" "Switched to light mode"
 fi
