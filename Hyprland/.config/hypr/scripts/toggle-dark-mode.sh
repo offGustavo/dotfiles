3dea81e5a04273c55fc02c63baf63f888af0e736
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-12-01 19:27:31 -0300

    feat(fuzzel): improve light and dark theme with `include`

--- Hyprland/.config/hypr/scripts/toggle-dark-mode.sh
+++ Hyprland/.config/hypr/scripts/toggle-dark-mode.sh
@@ -8,7 +8,7 @@ if [ "$current_scheme" = "'prefer-light'" ] || [ "$current_scheme" = "'default'"
     waybar -c ~/.config/hypr/waybar/config.jsonc -s ~/.config/hypr/waybar/style.css
     killall swaybg
     swaybg -i /home/gustavo/Pictures/Wallpapers/dharmx-Walls/mountain/a_rocky_cliff_with_trees_and_fog.jpg -m fill
-    ln -sf ~/.config/fuzzel/dark.ini ~/.config/fuzzel/fuzzel.ini
+    ln -sf ~/.config/fuzzel/dark.ini ~/.config/fuzzel/colors.ini
     # notify-send "Dark Mode" "Switched to dark mode"
 else
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
@@ -17,6 +17,6 @@ else
     waybar -c ~/.config/hypr/waybar/config.jsonc -s ~/.config/hypr/waybar/style.css
     killall swaybg
     swaybg -i /home/gustavo/Pictures/Wallpapers/dharmx-Walls/anime/a_cartoon_of_a_girl_standing_under_a_tree_with_purple_flowers.png -m fill
-    ln -sf ~/.config/fuzzel/light.ini ~/.config/fuzzel/fuzzel.ini
+    ln -sf ~/.config/fuzzel/light.ini ~/.config/fuzzel/colors.ini
     # notify-send "Light Mode" "Switched to light mode"
 fi
