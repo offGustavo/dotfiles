88c0a511a468fd0bd525f511d168d017d3db4989
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-12-02 11:46:36 -0300

    feat(hyprland): improve with a kill function

--- Hyprland/.config/hypr/scripts/toggle-dark-mode.sh
+++ Hyprland/.config/hypr/scripts/toggle-dark-mode.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 current_scheme=$(gsettings get org.gnome.desktop.interface color-scheme)
 
+kill_if_running() {
+    pgrep -x "$1" >/dev/null && killall "$1"
+}
+
 if [ "$current_scheme" = "'prefer-light'" ] || [ "$current_scheme" = "'default'" ]; then
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-    killall waybar
+    kill_if_running waybar
     ln -sf ~/.config/hypr/waybar/dark.css ~/.config/hypr/waybar/colors.css
     waybar -c ~/.config/hypr/waybar/config.jsonc -s ~/.config/hypr/waybar/style.css
-    killall swaybg
+    kill_if_running swaybg
     swaybg -i /home/gustavo/Pictures/Wallpapers/dharmx-Walls/mountain/a_rocky_cliff_with_trees_and_fog.jpg -m fill
     ln -sf ~/.config/fuzzel/dark.ini ~/.config/fuzzel/colors.ini
     # notify-send "Dark Mode" "Switched to dark mode"
 else
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
-    killall waybar
+    kill_if_running waybar
     ln -sf ~/.config/hypr/waybar/light.css ~/.config/hypr/waybar/colors.css
     waybar -c ~/.config/hypr/waybar/config.jsonc -s ~/.config/hypr/waybar/style.css
-    killall swaybg
+    kill_if_running swaybg
     swaybg -i /home/gustavo/Pictures/Wallpapers/dharmx-Walls/anime/a_cartoon_of_a_girl_standing_under_a_tree_with_purple_flowers.png -m fill
     ln -sf ~/.config/fuzzel/light.ini ~/.config/fuzzel/colors.ini
     # notify-send "Light Mode" "Switched to light mode"
