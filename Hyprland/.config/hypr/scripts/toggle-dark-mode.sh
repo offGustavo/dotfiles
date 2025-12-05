b3da953cbf01e80ac4108011906f6fac1d438611
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-12-05 10:04:07 -0300

    feat(hyprland): improve theme and swaybg start, also add random theme

--- Hyprland/.config/hypr/scripts/toggle-dark-mode.sh
+++ Hyprland/.config/hypr/scripts/toggle-dark-mode.sh
@@ -5,22 +5,32 @@ kill_if_running() {
   pgrep -x "$1" >/dev/null && killall "$1"
 }
 
+start_sway() {
+  # kill_if_running  swaybg
+  ~/.config/hypr/scripts/start_swaybg.lua
+}
+
+start_waybar() {
+  kill_if_running waybar
+  if [ "$current_scheme" = "'prefer-light'" ] || [ "$current_scheme" = "'default'" ]; then
+    ln -sf ~/.config/hypr/waybar/dark.css ~/.config/hypr/waybar/colors.css
+  else
+    ln -sf ~/.config/hypr/waybar/light.css ~/.config/hypr/waybar/colors.css
+  fi
+  waybar -c ~/.config/hypr/waybar/config.jsonc -s ~/.config/hypr/waybar/style.css
+}
+
 if [ "$current_scheme" = "'prefer-light'" ] || [ "$current_scheme" = "'default'" ]; then
   gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-  kill_if_running waybar
-  ln -sf ~/.config/hypr/waybar/dark.css ~/.config/hypr/waybar/colors.css
-  waybar -c ~/.config/hypr/waybar/config.jsonc -s ~/.config/hypr/waybar/style.css
-  kill_if_running swaybg
-  swaybg -i /home/gustavo/Pictures/Wallpapers/dharmx-Walls/mountain/a_rocky_cliff_with_trees_and_fog.jpg -m fill
   ln -sf ~/.config/fuzzel/dark.ini ~/.config/fuzzel/colors.ini
-  # notify-send "Dark Mode" "Switched to dark mode"
+
+  start_waybar
+  start_sway
+  notify-send "Dark Mode" "Switched to dark mode"
 else
   gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
-  kill_if_running waybar
-  ln -sf ~/.config/hypr/waybar/light.css ~/.config/hypr/waybar/colors.css
-  waybar -c ~/.config/hypr/waybar/config.jsonc -s ~/.config/hypr/waybar/style.css
-  kill_if_running swaybg
-  swaybg -i /home/gustavo/Pictures/Wallpapers/dharmx-Walls/anime/a_cartoon_of_a_girl_standing_under_a_tree_with_purple_flowers.png -m fill
   ln -sf ~/.config/fuzzel/light.ini ~/.config/fuzzel/colors.ini
-  # notify-send "Light Mode" "Switched to light mode"
+  start_waybar 
+  start_sway
+  notify-send "Light Mode" "Switched to light mode"
 fi
