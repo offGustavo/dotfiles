3edb1d9c7e1efb1449cdd8bdf16e99d6d2997229
Gustavo <gustavo@nixos.com>
2025-12-11 12:35:17 -0300

    feat(vim): enalbe mini-pairs in lazyvim

--- Neovim/.config/nvim/lua/plugins/mini-pairs.lua
+++ Neovim/.config/nvim/lua/plugins/mini-pairs.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-mini/mini.pairs",
   event = "VeryLazy",
-  enabled = false,
+  enabled = true,
   opts = {
     modes = { insert = true, command = true, terminal = false },
     -- skip autopair when next character is one of these
@@ -17,12 +17,12 @@ return {
       ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
       ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
       ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
-      ["<"] = { action = "open", pair = "<>", neigh_pattern = "[^\\]." },
+      -- ["<"] = { action = "open", pair = "<>", neigh_pattern = "[^\\]." },
 
       [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
       ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
       ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
-      [">"] = { action = "close", pair = "<>", neigh_pattern = "[^\\]." },
+      -- [">"] = { action = "close", pair = "<>", neigh_pattern = "[^\\]." },
 
       ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
       ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
