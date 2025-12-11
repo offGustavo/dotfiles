ff5ceb6a5c7a6235765a7e23c13428117e5134e7
Gustavo <gustavo@nixos.com>
2025-12-10 21:22:20 -0300

    feat(vim): map local leader

--- Neovim/.config/nvim/lua/config/options.lua
+++ Neovim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.cmd([[
+  let maplocalleader = "\<BS>"
+]])
+
 vim.g.snacks_animate = false
 
 -- vim.opt.scrolloff = 999
