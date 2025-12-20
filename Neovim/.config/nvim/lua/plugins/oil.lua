e34d8d02c9abf686fccf5022d501839e93033d92
Gustavo <gustavo@nixos.com>
2025-12-20 17:36:28 -0300

    feat(vim): add path to winbar in oil

--- Neovim/.config/nvim/lua/plugins/oil.lua
+++ Neovim/.config/nvim/lua/plugins/oil.lua
@@ -7,6 +7,17 @@ return {
     { "<leader><Cr>", "<Cmd>Oil --float<Cr>", desc = "Oil" },
   },
   config = function()
+   -- Declare a global function to retrieve the current directory
+    function _G.get_oil_winbar()
+      local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+      local dir = require("oil").get_current_dir(bufnr)
+      if dir then
+        return vim.fn.fnamemodify(dir, ":~")
+      else
+        -- If there is no current directory (e.g. over ssh), just show the buffer name
+        return vim.api.nvim_buf_get_name(0)
+      end
+    end
     require("oil").setup({
       -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
       -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
@@ -35,6 +46,7 @@ return {
         conceallevel = 3,
         concealcursor = "nvic",
         relativenumber = false,
+        winbar = "%!v:lua.get_oil_winbar()",
       },
       -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
       delete_to_trash = true,
