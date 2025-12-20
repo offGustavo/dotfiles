cc678ee4aabd20d8149a01a0d9cfbd10f66d65fd
Gustavo <gustavo@nixos.com>
2025-12-20 14:31:42 -0300

    feat(wezterm): update script in neovim and improve keymaps

---
+++ Wezterm/.config/wezterm/theme.lua
@@ -0,0 +1,43 @@
+return {
+  cursor_bg = '#c0caf5',
+  scrollbar_thumb = '#16161e',
+  selection_fg = '#c0caf5',
+  selection_bg = '#283457',
+  cursor_border = '#c0caf5',
+  split = '#15161e',
+  background = '#1a1b26',
+  ansi = { '#15161e', '#f7768e', '#9ece6a', '#e0af68', '#7aa2f7', '#bb9af7', '#7dcfff', '#a9b1d6' },
+  brights = { '#414868', '#ff899d', '#9fe044', '#faba4a', '#8db0ff', '#c7a9ff', '#a4daff', '#c0caf5' },
+  tab_bar = {
+    background = '#292e42',
+    active_tab = {
+      underline = 'None',
+      intensity = 'Normal',
+      fg_color = '#15161e',
+      bg_color = '#7aa2f7',
+      italic = false,
+      strikethrough = false,
+    },
+    new_tab_hover = {
+      fg_color = '#3b4261',
+      bg_color = '#15161e',
+      italic = false,
+    },
+    inactive_tab = {
+      fg_color = '#3b4261',
+      bg_color = '#292e42',
+    },
+    new_tab = {
+      fg_color = '#3b4261',
+      bg_color = '#292e42',
+    },
+    inactive_tab_hover = {
+      fg_color = '#3b4261',
+      bg_color = '#15161e',
+      italic = true,
+    },
+  },
+  compose_cursor = '#e0af68',
+  foreground = '#c0caf5',
+  cursor_fg = '#1a1b26',
+}
