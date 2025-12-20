83769684518bacc7484fd82155d13f30b2fafcc4
Gustavo <gustavo@nixos.com>
2025-12-20 17:38:19 -0300

    feat(vim): remove unused lua modules

--- Neovim/.config/nvim/init.lua
+++ Neovim/.config/nvim/init.lua
@@ -2,7 +2,7 @@
 require("config.lazy")
 
 -- Custom Things
-require("fish.multi_marks")
+-- require("fish.multi_marks")
 require("fish.commads")
 require("fish.filetypes")
 
@@ -10,12 +10,21 @@ require("fish.filetypes")
 require("fish.neovide")
 require("fish.vscode")
 
--- Theme
-require("fish.theme")
+if os.getenv("DISPLAY") then
+  -- Theme
+  require("fish.theme")
+  -- require("themes.compline").setup()
+else
+  vim.cmd([[
+      colorscheme default
+      set notermguicolors
+    ]])
+end
 
 if vim.uv.os_uname().sysname == "Windows_NT" then
 else
-  require("fish.kitty")
-  require("fish.tmux")
-  require("fish.lazygit")
+  require("fish.wezterm")
+  -- require("fish.kitty")
+  -- require("fish.tmux")
+  -- require("fish.lazygit")
 end
