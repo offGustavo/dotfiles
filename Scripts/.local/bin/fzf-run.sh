c6e0bb4b15e9814f67e35a8fdf204c3c4c9833cb
Gustavo <gustavo@nixos.com>
2025-12-24 11:45:50 -0300

    feat(scripts): add new scripts and remove dup options

--- Scripts/.local/bin/fzf-run.sh
+++ Scripts/.local/bin/fzf-run.sh
@@ -93,30 +93,9 @@ COMMAND_STR=$(
   ) |
     fzf +s -x -d '\034' --nth ..3 --with-nth 3 \
       --preview "$0 describe {1} {2}" \
-      --preview-window=down:3:wrap --ansi \
       --info=inline-right \
       --ansi \
       --layout=reverse \
-      --border=rounded \
-      --style=full \
-      --margin=3% \
-      --color=bg+:#1a1b26 \
-      --color=bg:#16161e \
-      --color=border:#27a1b9 \
-      --color=fg:#c0caf5 \
-      --color=gutter:#16161e \
-      --color=header:#ff9e64 \
-      --color=hl+:#2ac3de \
-      --color=hl:#2ac3de \
-      --color=info:#545c7e \
-      --color=marker:#ff007c \
-      --color=pointer:#ff007c \
-      --color=prompt:#2ac3de \
-      --color=query:#c0caf5:regular \
-      --color=scrollbar:#27a1b9 \
-      --color=separator:#ff9e64 \
-      --color=spinner:#ff007c \
-      --preview-window down \
       --bind ctrl-d:preview-half-page-down,ctrl-u:preview-half-page-up
   kill -9 "$(<"$PIDFILE")" | tail -n1
 ) || exit 1
