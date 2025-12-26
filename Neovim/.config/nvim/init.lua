b5061b06e344ff86b27ac2e80bcee79531f25a65
Gustavo <gustavo@nixos.com>
2025-12-26 20:30:59 -0300

    feat(vim): clean lvim config

--- Neovim/.config/nvim/init.lua
+++ Neovim/.config/nvim/init.lua
@@ -10,21 +10,21 @@ require("fish.filetypes")
 require("fish.neovide")
 require("fish.vscode")
 
-if os.getenv("DISPLAY") then
+-- if os.getenv("DISPLAY") then
   -- Theme
-  require("fish.theme")
-  -- require("themes.compline").setup()
-else
-  vim.cmd([[
-      colorscheme default
-      set notermguicolors
-    ]])
-end
+  -- require("fish.theme")
+  require("themes.compline").setup()
+-- else
+--   vim.cmd([[
+--       colorscheme default
+--       set notermguicolors
+--     ]])
+-- end
 
 if vim.uv.os_uname().sysname == "Windows_NT" then
 else
   require("fish.wezterm")
-  -- require("fish.kitty")
+  require("fish.kitty")
   -- require("fish.tmux")
   -- require("fish.lazygit")
 end
