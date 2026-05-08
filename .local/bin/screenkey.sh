c01e13b034c3c7693cefe46b7230b07592ad5cbe
Gustavo <change_later@example.com>
2026-05-07 22:20:52 -0300

    feat(scripts): fix screenkey script

--- .local/bin/screenkey.sh
+++ .local/bin/screenkey.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 # Check if screenkey is running
-if pgrep -x "screenkey" > /dev/null; then
+PID=$(pgrep screenkey)
+if [[ -n "$PID" ]]; then
     # Kill all screenkey processes
-    pkill -x screenkey
+    kill "$PID"
 else
     # Start screenkey detached from terminal
-    nohup /usr/bin/screenkey --geometry 200x400+1700+0 -s medium --opacity 0.4 > /dev/null 2>&1 &
+    nohup screenkey --geometry 200x400+1700+0 -s medium --opacity 0.4 > /dev/null 2>&1 &
 fi
