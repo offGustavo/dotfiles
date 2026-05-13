3348f59c725278cd1c61a7992b06b47c9a00c4cd
Gustavo <change_later@example.com>
2026-05-13 12:27:17 -0300

    feat(i3): fix i3 theme config

--- .config/i3/scripts/toggle-dark-mode.sh
+++ .config/i3/scripts/toggle-dark-mode.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 current_scheme="$1"
 
-I3_CONFIG=~/.config/i3/config
+THEME_FILE=~/.config/i3/config
+BASE="include ~/.config/i3/i3.config"
 
 if [ "$current_scheme" = "light" ]; then
+  # Wallpaper
+  xwallpaper --zoom ~/Pictures/Wallpapers/dharmx-Walls/cherry/a_tree_with_pink_flowers.jpg
 
- # Wallpaper
- xwallpaper --zoom ~/Pictures/Wallpapers/dharmx-Walls/cherry/a_tree_with_pink_flowers.jpg
+  cat > "$THEME_FILE" <<'EOF'
+# WARN: AUTOGEN FILE
+# Do not edit there, use the included file to edit the config
+# I3 Config
+include ~/.config/i3/i3.config
 
-
-  # Remove existing theme block if present
-  sed -i '/^# Tokyo Night/,/^$/d' "$I3_CONFIG"
-  # Add light theme
-  cat >>"$I3_CONFIG" <<'EOF'
 # Tokyo Night Day Theme
 set $fg #3760bf
 set $bg #e1e2e7
@@ -22,17 +23,16 @@ set $red #f52a65
 set $green #587539
 set $purple #7847bd
 EOF
+
 else
+  # Wallpaper
+  xwallpaper --zoom /home/gustavo/Pictures/Wallpapers/dharmx-Walls/spam/a_street_with_signs_and_lights_01.png
 
- # Wallpaper
- # xwallpaper --zoom ~/Pictures/Wallpapers/dharmx-Walls/unsorted/a_black_background_with_purple_and_blue_spots.png
- xwallpaper --zoom /home/gustavo/Pictures/Wallpapers/dharmx-Walls/spam/a_street_with_signs_and_lights_01.png
- # xwallpaper --zoom /home/gustavo/Pictures/Wallpapers/dharmx-Walls/centered/a_blue_sky_with_clouds.png
-
-  # Remove existing theme block if present
-  sed -i '/^# Tokyo Night/,/^$/d' "$I3_CONFIG"
-  # Add dark theme
-  cat >>"$I3_CONFIG" <<'EOF'
+  cat > "$THEME_FILE" <<'EOF'
+# WARN: AUTOGEN FILE
+# Do not edit there, use the included file to edit the config
+# I3 Config
+include ~/.config/i3/i3.config
 # Tokyo Night Theme
 set $fg #c0caf5
 set $bg #1a1b26
@@ -44,5 +44,4 @@ set $purple #c678dd
 EOF
 fi
 
-# Reload i3 to apply changes
 i3-msg reload
