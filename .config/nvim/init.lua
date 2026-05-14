0703f7667180f14c3ed8f31bbb6e33f674b12bb1
Gustavo <change_later@example.com>
2026-05-14 11:34:25 -0300

    feat(vim+kitty): move SCROLL_MODE logic to init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,24 @@
+-- Set a temp theme here to prevent light/dark flicker
+if vim.o.background == "dark" then
+  vim.cmd.colorscheme("tokyo")
+else
+  vim.cmd.colorscheme("tokyo-day")
+end
+
+-- Kitty scroll mode
+if os.getenv("SCROLL_MODE") then
+  vim.cmd([[
+    nmap q <Cmd>qa!<CR>
+    xmap q <Cmd>qa!<CR> 
+    nnoremap yy "+yy<Cmd>qa!<Cr>
+    xmap y "+y<Cmd>qa!<Cr>
+    set laststatus=0 nonu nornu signcolumn=no cursorline cmdheight=0
+    $ 
+  ]])
+  -- NOTE: Stop vimrc here
+  return
+end
+
 -- Map Leader and Local Leader
 vim.cmd([[
   " <space> as leader
@@ -9,20 +30,13 @@ vim.cmd([[
 -- Our Global thing
 _G.fish = {}
 
--- Set a temp theme here to prevent light/dark flicker
-if vim.o.background == "dark" then
-    vim.cmd.colorscheme("tokyo")
-else
-    vim.cmd.colorscheme("tokyo-day")
-end
-
 -- Basic keymaps for Nvim (if lazy fails for some reason)
 vim.cmd([[
-    nmap <M-o> :fin<Space>
-    nmap <M-s> :grep<Space>
-    nmap <M-b> :b<Space>
-    nmap <M-e> :Ex<Cr>
-    nmap <leader>vc :e $MYVIMRC<Cr>
+  nmap <M-o> :fin<Space>
+  nmap <M-s> :grep<Space>
+  nmap <M-b> :b<Space>
+  nmap <M-e> :Ex<Cr>
+  nmap <leader>vc :e $MYVIMRC<Cr>
 ]])
 
 -- Disable Custom Nix/Arch FZF.vim
